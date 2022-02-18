7be192150d3f1096ccaefeaec11d1c999a64811a
Ajay Kumar <ajjaystyles@gmail.com>
2022-02-18 12:49:45 +0530

    second

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var singin=document.querySelector('.signin');
 var singup=document.querySelector('.signup');
 var btn1=document.querySelector('#one');
 var btn2=document.querySelector('#two');
+var stop=document.querySelector('.stop');
 
 singup.style.display='none';
 
@@ -13,4 +14,12 @@ btn1.addEventListener('click',()=>{
 btn2.addEventListener('click',()=>{
     singin.style.display='block';
     singup.style.display='none';
+})
+
+var myMusic = document.querySelector('#music');
+    myMusic.play();
+    
+stop.addEventListener('click',()=>{
+    myMusic.pause();
+    stop.style.display='none';
 })
